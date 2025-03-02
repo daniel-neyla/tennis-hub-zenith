7c052ee7d515d18f39e4d857f5df6784a085717e
Daniel Hornung <daniel@dans-airmac-2.local>
2025-03-02 22:55:23 +0000

    Added route to results page

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Index from "./pages/Index";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import NotFound from "./pages/NotFound";
+import Results from "./pages/Results";
 
 const queryClient = new QueryClient();
 
@@ -21,6 +22,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/results" element={<Results />} />
           <Route path="/news" element={<NotFound />} /> {/* Placeholder for future News page */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
